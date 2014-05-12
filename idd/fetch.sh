822d6f1c4f9c56e4c370ecb906cca05c8571295c
Ryan May <rmay31@gmail.com>
2014-05-12 17:55:30 -0600

    Add configuration build configs and artifacts (and scripts).

---
+++ idd/fetch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/Unidata/TdsConfig/master/idd/config.zip
+jar -x config.zip
---
+++ remotetest/fetch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/Unidata/TdsConfig/master/remotetest/config.zip
+jar -x config.zip
