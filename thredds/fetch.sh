55ff0be7aedc6ad03d01e8c057307f25bc008241
Sean Arms <sarms@ucar.edu>
2015-12-01 12:05:08 -0700

    added gfs 0.25 and hrrr from ncep to idd and thredds configs

--- thredds/fetch.sh
+++ thredds/fetch.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-unamestr=`uname`
-if [[ "$unamestr" == 'SunOS' ]]; then
-   PATH=/opt/csw/bin:/opt/jdk/bin:$PATH
-fi
-
 {
-        wget --no-check-certificate https://raw.githubusercontent.com/Unidata/TdsConfig/master/thredds/config.zip -O config.zip
-        jar xf config.zip
-        exit
+	wget --no-check-certificate https://raw.githubusercontent.com/Unidata/TdsConfig/master/thredds/config.zip -O config.zip
+	jar xf config.zip
+	exit
 }
